f7dfda35b0d499c174c2bd4fb706948bd5f38426
Shouq Almutairi <shouqalmutaiiri8@gmail.com>
2025-09-21 16:40:12 +0300

    creating the file structure, and the first method of the module of database interactions

---
+++ bank/db_interaction.py
@@ -0,0 +1,16 @@
+import csv 
+
+data_list = []  
+
+def open_file(file_name):
+    # from https://www.geeksforgeeks.org/python/working-csv-files-python/
+    with open(file_name, mode='r') as file:
+        csv_reader = csv.DictReader(file)  
+
+
+        for row in csv_reader:
+            data_list.append(row)
+            
+
+
+
