949056f8b2bfe02ade4765019991cac9ad7e002a
Shouq Almutairi <shouqalmutaiiri8@gmail.com>
2025-09-21 17:36:29 +0300

    testing the method get_row from file_management

--- test/test_file_management.py
+++ test/test_file_management.py
@@ -7,3 +7,11 @@ class TestDBInteraction(unittest.TestCase):
     def test_open_file(self):
         with self.assertRaises(FileNotFoundError):
             file.open_file("non_existent_file.csv")
+            
+    def test_get_row(self):
+        file.data_list = [
+{'account_id': 10004, 'frst_name': 'stacey', 'last_name': 'abrams','password': 'DEU8_qw3y72$', 'balance_checking': 2000, 'balance_savings': 20000}]
+        self.assertEqual(file.get_row(10004), file.data_list[0])
+        # testing when not found
+        self.assertEqual(file.get_row(10009), -1)
+    
