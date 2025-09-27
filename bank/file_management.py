9bf687b78082e6ba93864df2558a06eacf42e05d
Shouq Almutairi <shouqalmutaiiri8@gmail.com>
2025-09-27 21:10:05 +0300

    handling exceptions if the file is empty or had only fields

--- bank/file_management.py
+++ bank/file_management.py
@@ -34,6 +34,8 @@ class FileManagement():
 
             self.convert_data_type()
 
+            if not self.fields:
+                raise ValueError("The file is empty! it should have at least field names")
 
 
 
@@ -80,7 +82,7 @@ class FileManagement():
 
     def get_last_row_id(self):
         if not self.data_list:
-            raise ValueError("The file is empty!")
+            return 9999 #so that the first customer is 10000
         return self.data_list[-1]["account_id"]
 
     def convert_data_type(self):
